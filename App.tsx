93353ed06b63d2f9d7555648d0cbe31bbffe6695
mgoldman10 <mike@mike-goldman.com>
2025-11-30 11:42:12 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { AssessmentData, Company, ParticipantResponse, User } from './types';
 import { getAssessmentData } from './services/geminiService';
